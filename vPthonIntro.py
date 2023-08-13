c802354abce7ecabc0a491ce42cada45257979e2
Dave Olson <dolson56@cox.net>
2023-08-12 17:09:13 -0700

    second commit

---
+++ vPthonIntro.py
@@ -0,0 +1,10 @@
+from vpython import *
+from time import *
+print ("Hello")
+ball=sphere(color = color.magenta)
+sleep(2)
+ball.color = color.blue
+sleep(3)
+ball.color = color.green
+while True: 
+    pass
